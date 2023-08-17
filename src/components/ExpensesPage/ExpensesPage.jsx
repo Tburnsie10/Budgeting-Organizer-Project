37306dc40bf553e6bdd16959e6f7a4b95f131a4f
Tyler Burns <Tburnsie10@gmail.com>
2023-08-16 19:47:49 -0500

    got the date amount and name setup

--- src/components/ExpensesPage/ExpensesPage.jsx
+++ src/components/ExpensesPage/ExpensesPage.jsx
@@ -3,6 +3,9 @@ import React, {useEffect} from "react";
 import LogOutButton from "../LogOutButton/LogOutButton";
 import { useSelector, useDispatch } from "react-redux";
 import { Table } from "reactstrap";
+import './ExpensesPage.css'
+
+
 function ExpensesPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const expenses = useSelector((store) => store.expenses);
@@ -12,15 +15,18 @@ function ExpensesPage() {
 useEffect(() => {
   dispatch({type:'GET_EXPENSES'})
 },[])
+console.log(expenses)
 
   function addRow(event) {
     event.preventDefault();
-    let inputs = event.parentElement.querySelectorAll('input')
+    console.log(event)
+    let inputs = event.target.parentElement.querySelectorAll('input')
+    console.log(inputs)
     let name = inputs[0].value
     let amount = inputs[1].value
     let date = inputs[2].value
 
-    let obj = {type:'ADD_EXPENSE', payload:{name, amount, date}}
+    let obj = {type:'ADD_EXPENSE', payload:[name, amount, date]}
     dispatch(obj)
     console.log(expenses)
   }
@@ -35,32 +41,33 @@ useEffect(() => {
 
   return (
     <div className="container">
-      <div>Total Expenses:{expenseTotal()}</div>
       <h2>Expenses</h2>
       <Table>
-        <thead>
+        <thead >
           <tr>
             <th>Name</th>
             <th>Amount</th>
             <th>Date</th>
           </tr>
+          
         </thead>
         <tbody>
             {expenses.map(expense => {
                 return (<tr>
-                    <td>{expense.name}</td>
+                    <td>{expense.expense_name}</td>
                     <td>{expense.amount}</td>
-                    <td>{expense.date}</td>
+                    <td>{expense.expense_date.split('T')[0]}</td>
                   </tr>)
             })}
         </tbody>
-        <div id="input">
+      </Table>
+      <div className="top">Total Expenses:{expenseTotal()}</div>
+      <div id="input">
           <input placeholder="name"></input>
           <input placeholder="amount"></input>
           <input placeholder="date"></input>
           <button onClick={addRow}>Add Row</button>
         </div>
-      </Table>
     </div>
   );
 }
