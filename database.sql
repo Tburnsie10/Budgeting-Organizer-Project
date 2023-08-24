cee4e7376c59daaa52b2ed0ec13d9a031d24ecee
Tyler Burns <Tburnsie10@gmail.com>
2023-08-23 21:57:42 -0500

    good

--- database.sql
+++ database.sql
@@ -4,7 +4,24 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    id SERIAL PRIMARY KEY,
+    username character varying(80) NOT NULL UNIQUE,
+    password character varying(1000) NOT NULL
+);
+
+
+CREATE TABLE expenses (
+    id SERIAL PRIMARY KEY,
+    expense_name character varying(255),
+    amount numeric(10,2),
+    expense_date date,
+    user_id integer REFERENCES "user"(id)
+);
+
+CREATE TABLE income (
+    id SERIAL PRIMARY KEY,
+    income_name character varying(255),
+    amount numeric(10,2),
+    income_date date,
+    user_id integer REFERENCES "user"(id)
 );
